d72192c987718274050f88bd1e28bb77f843fdee
cyrus-shahrivar <cyrus.shahrivar@gmail.com>
2022-02-27 19:28:57 -0500

    test: DRY tests

--- src/GreetingComponent/GreetingHelpers.test.js
+++ src/GreetingComponent/GreetingHelpers.test.js
@@ -2,42 +2,50 @@ import { getGreeting } from "./GreetingHelpers"
 
 describe("#getGreeting", () => {
   test("should return Good Morning if time is between midnight-noon", () => {
+    const morningGreeting = "Good Morning"
+
     const midnight = getGreeting(0)
     const midMorning = getGreeting(5)
     const noon = getGreeting(12)
 
-    expect(midnight).toBe("Good Morning")
-    expect(midMorning).toBe("Good Morning")
-    expect(noon).not.toBe("Good Morning")
+    expect(midnight).toBe(morningGreeting)
+    expect(midMorning).toBe(morningGreeting)
+    expect(noon).not.toBe(morningGreeting)
   })
 
   test("should return Good Afternoon if time is between noon-5pm", () => {
+    const afternoonGreeting = "Good Afternoon"
+
     const noon = getGreeting(12)
     const midAfternoon = getGreeting(16)
     const at5PM = getGreeting(17)
 
-    expect(noon).toBe("Good Afternoon")
-    expect(midAfternoon).toBe("Good Afternoon")
-    expect(at5PM).not.toBe("Good Afternoon")
+    expect(noon).toBe(afternoonGreeting)
+    expect(midAfternoon).toBe(afternoonGreeting)
+    expect(at5PM).not.toBe(afternoonGreeting)
   })
 
   test("should return Good Evening if time is between 5pm-8pm", () => {
+    const eveningGreeting = "Good Evening"
+
     const at5PM = getGreeting(17)
     const midEvening = getGreeting(18)
     const at8PM = getGreeting(20)
 
-    expect(at5PM).toBe("Good Evening")
-    expect(midEvening).toBe("Good Evening")
-    expect(at8PM).not.toBe("Good Evening")
+    expect(at5PM).toBe(eveningGreeting)
+    expect(midEvening).toBe(eveningGreeting)
+    expect(at8PM).not.toBe(eveningGreeting)
   })
 
   test("should return Good Night if time is between 8pm-midnight", () => {
+    const nightGreeting = "Good Night"
+
     const at8PM = getGreeting(20)
     const past8PM = getGreeting(21)
     const midnight = getGreeting(0)
 
-    expect(at8PM).toBe("Good Night")
-    expect(past8PM).toBe("Good Night")
-    expect(midnight).not.toBe("Good Night")
+    expect(at8PM).toBe(nightGreeting)
+    expect(past8PM).toBe(nightGreeting)
+    expect(midnight).not.toBe(nightGreeting)
   })
 })
